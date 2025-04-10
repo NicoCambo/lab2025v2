25979b7f26f31b39df9336730e98110af6a31080
NicoCambo <cambonicola@gmail.com>
2025-04-10 18:50:29 +0200

    sembraok???

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Path
 from app.models.book import Book
 from app.data.books import books
+from typing import Annotated
 
 router = APIRouter(prefix="/books")
 
@@ -11,6 +12,9 @@ def get_all_books() -> list[Book]:
     return list(books.values())
 
 @router.get("/{id}")
-def get_book_by_id(id: int) -> Book:
+def get_book_by_id(id: Annotated[int, Path(description="The ID of the book to get")]) -> Book:
     """Returns the book with the given id."""
-    return books[id]
+    try:
+        return books[id]
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Book not found")
