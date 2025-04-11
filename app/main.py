4b746cc36842f62117e058980e7e630afdb8d999
NicoCambo <cambonicola@gmail.com>
2025-04-11 11:54:49 +0200

    risolto quasi tutto.

--- app/main.py
+++ app/main.py
@@ -1,26 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import books
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 
 app = FastAPI()
 
 app.include_router(books.router, tags=["books"])
-
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/",response_class=HTMLResponse)
-def home():
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <body>
-            <h1>Hello World!</h1>
-        </body>
-    </html>
-    """
-    return html
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html"
+    )
+
+
 
 if __name__ == "__main__":
     import uvicorn
